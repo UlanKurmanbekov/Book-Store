fbe1e3f17a80b7c8ea39ff201fd61fb5a629ebd4
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-03 00:28:06 +0600

    Update BookViewSet to use IsOwnerOrReadOnly permission

--- store/views.py
+++ store/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrReadOnly
 from store.serializers import BookSerializer
 
 
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrReadOnly]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']

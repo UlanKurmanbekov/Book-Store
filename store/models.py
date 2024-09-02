2f8624655469ef4c49767fc0a68a087dc2aba7e1
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-03 01:30:21 +0600

    Add UserBookRelation model

--- store/models.py
+++ store/models.py
@@ -6,7 +6,27 @@ class Book(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
-    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='owned_books')
+    readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books_read')
 
     def __str__(self):
         return f'ID {self.id}: {self.name}'
+
+
+class UserBookRelation(models.Model):
+    RATE_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    like = models.BooleanField(default=False)
+    in_bookmarks = models.BooleanField(default=False)
+    rate = models.PositiveSmallIntegerField(choices=RATE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.book.name} - rate {self.rate}'

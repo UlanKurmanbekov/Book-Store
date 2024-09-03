11ef271ce69c7b14aef7efd1e047540835d87cd7
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-03 16:23:46 +0600

    Add likes_count field to BookSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,15 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Book, UserBookRelation
 
 
 class BookSerializer(ModelSerializer):
+    likes_count = serializers.IntegerField(read_only=True)
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ('id', 'name', 'price', 'author_name', 'likes_count')
 
 
 class UserBookRelationSerializer(ModelSerializer):

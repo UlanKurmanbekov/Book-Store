d0fcd66675ad2c219c048d438f605928c306ac14
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-06 03:33:36 +0600

    Update owner_name field in BookSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -15,7 +15,7 @@ class BookSerializer(ModelSerializer):
     likes_count = serializers.IntegerField(read_only=True)
     rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
     discounted_price = serializers.DecimalField(max_digits=7, decimal_places=2, read_only=True)
-    owner_name = serializers.CharField(source='owner.username', default='', read_only=True)
+    owner_name = serializers.CharField(read_only=True)
     readers = BookReaderSerializer(many=True, read_only=True)
 
     class Meta:

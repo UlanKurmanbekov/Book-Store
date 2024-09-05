f530d7d99af13f58b349183a74679b2665540b24
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-06 04:43:09 +0600

    Add save method in UserBookRelation model

--- store/models.py
+++ store/models.py
@@ -35,3 +35,15 @@ class UserBookRelation(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.book.name} - rate {self.rate}'
+
+    def save(self, *args, **kwargs):
+        from store.utils import set_rating
+
+        creating = not self.pk
+        ord_rate = self.rate
+
+        super().save(*args, **kwargs)
+
+        new_rate = self.rate
+        if ord_rate != new_rate or creating:
+            set_rating(self.book)

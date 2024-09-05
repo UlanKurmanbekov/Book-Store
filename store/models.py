bd238aafba223d6859d1b3aa2ae78483e83befdf
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-05 19:11:04 +0600

    Add discount field to Book model

--- store/models.py
+++ store/models.py
@@ -1,13 +1,17 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
 class Book(models.Model):
+    DISCOUNT_VALIDATE = [MinValueValidator(0), MaxValueValidator(100)]
+
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     author_name = models.CharField(max_length=255)
     owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='owned_books')
     readers = models.ManyToManyField(User, through='UserBookRelation', related_name='books_read')
+    discount = models.PositiveSmallIntegerField(default=0, blank=True, validators=DISCOUNT_VALIDATE)
 
     def __str__(self):
         return f'ID {self.id}: {self.name}'

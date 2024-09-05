aaf9f53be9b4dbdc490cff2d6635c2ce512af026
Ulan Kurmanbekov <ulankurmanbekov83@gmail.com>
2024-09-05 22:52:21 +0600

    Add owner_name and readers fields to BookSerializer

--- store/serializers.py
+++ store/serializers.py
@@ -1,17 +1,30 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from store.models import Book, UserBookRelation
 
 
+class BookReaderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
+
+
 class BookSerializer(ModelSerializer):
     likes_count = serializers.IntegerField(read_only=True)
     rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
     discounted_price = serializers.DecimalField(max_digits=7, decimal_places=2, read_only=True)
+    owner_name = serializers.CharField(source='owner.username', default='', read_only=True)
+    readers = BookReaderSerializer(many=True, read_only=True)
 
     class Meta:
         model = Book
-        fields = ('id', 'name', 'price', 'discounted_price', 'author_name', 'likes_count', 'rating')
+        fields = (
+            'id', 'name', 'price',
+            'discounted_price', 'author_name', 'likes_count',
+            'rating', 'owner_name',  'readers'
+        )
 
 
 class UserBookRelationSerializer(ModelSerializer):
